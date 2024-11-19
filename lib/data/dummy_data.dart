2981b5d6e66a5e8dfbed72c30e6484e7001a103d
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2024-11-19 21:58:44 +0700

    init

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:meals/category.dart';
+
+const availableCategories = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Vegetarian',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Vegan',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Dessert',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Seafood',
+    color: Colors.orange,
+  ),
+];
