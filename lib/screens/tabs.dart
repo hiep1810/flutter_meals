c874743b4405302ce21a6a58dd0992682151d654
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2024-11-29 23:49:43 +0700

    feat: add drawer to application

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -52,7 +53,6 @@ class _TabsScreenState extends State<TabsScreen> {
 
     if (_selectedIndex == 1) {
       activePage = MealsScreen(
-          title: 'Favorites',
           meals: _favoriteMeals,
           onToggleFavorite: _toggleFavorite);
       activePageTitle = 'Favorites';
@@ -62,6 +62,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
+      drawer: const MainDrawer(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _selectPage,
